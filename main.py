1aebbcfce1535962001a023624a47c1b5941813b
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-02-13 19:17:29 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import telebot
+from TOKEN import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "beep!")
+
+
+@bot.message_handler(func=lambda message: message.text=="keyboard")
+def echo_message(message):
+    # bot.reply_to(message, message.text)
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = telebot.types.KeyboardButton('a')
+    itembtn2 = telebot.types.KeyboardButton('v')
+    itembtn3 = telebot.types.KeyboardButton('d')
+    markup.add(itembtn1, itembtn2, itembtn3)
+    bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
